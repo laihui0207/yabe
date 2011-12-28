4227a45510be874bffecb125968ae7f1430fc683
laihui0207 <laihui0207gm@gmail.com>
2011-12-29 00:36:12 +0800

    admin

--- app/controllers/Admin.java
+++ app/controllers/Admin.java
@@ -19,4 +19,23 @@ public class Admin extends Controller {
 		List<Post> posts=Post.find("author.email",user).fetch();
      		render(posts); 
 	}
+	public static void form() { 
+      		render(); 
+	}
+	public static void save(String title,String content,String tags) { 
+		User author=User.find("byEmail",Security.connected()).first();
+	 	Post post = new Post(author,title,content);	
+		for(String tag: tags.split("\\s+")){
+			if (tag.trim().length()>0){
+			       post.tags.add(Tag.findOrCreateByName(tag));
+			}
+		}
+
+		validation.valid(post);
+		if(validation.hasErrors()){
+			render("@form",post);
+		}
+		post.save();
+		index();
+	}
 }
